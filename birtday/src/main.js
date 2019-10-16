c54eecbac63ebf25095c14b5e2d091785fd72e62
Bob-Dole <abcde66@qq.com>
2019-10-16 17:57:16 +0800

    修改了main.js

--- birtday/src/main.js
+++ birtday/src/main.js
@@ -5,14 +5,14 @@ import store from './store'
 import axios from 'axios'
 import MintUI from "mint-ui"
 
-import "mint-ui/lib/style.css"
+// import "mint-ui/lib/style.css"
 Vue.use(MintUI)
 
 Vue.prototype.axios=axios
 
 Vue.config.productionTip = false
-import './lib/mui/css/mui.css'
-import './lib/mui/css/icons-extra.css';
+// import './lib/mui/css/mui.css'
+// import './lib/mui/css/icons-extra.css';
 
 new Vue({
   router,
